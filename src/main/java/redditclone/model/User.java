d957b6c0ebf84e0d7fb202fff5321d87943e9a3a
johnUfo <muydinovumidjon17@gmail.com>
2024-12-03 10:51:05 +0500

    databased changed from mysql to postgres

--- src/main/java/redditclone/model/User.java
+++ src/main/java/redditclone/model/User.java
@@ -1,9 +1,6 @@
 package redditclone.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 
@@ -18,6 +15,7 @@ import java.time.Instant;
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "users")
 public class User {
 
     @Id
